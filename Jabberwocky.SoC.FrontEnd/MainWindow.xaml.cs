a1f57d0a5d1ed745a9480b473f7860b435e6ce9b
Benjamin Toynbee <toyners@hotmail.com>
2016-10-16 21:08:24 +1100

    Add project files.

---
+++ Jabberwocky.SoC.FrontEnd/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Jabberwocky.SoC.FrontEnd
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void CreateMap(Object sender, RoutedEventArgs e)
+    {
+      this.Viewport.Source = new Uri(@"C:\Projects\Unity Projects\SoC_Content\content.html");
+    }
+  }
+}
